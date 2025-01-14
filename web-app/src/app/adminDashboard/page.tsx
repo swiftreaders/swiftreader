5a6ac3eb2079cb8f1133a634402ae79778a00e21
SriharshaV04 <sriharsha.vitta@gmail.com>
2025-01-14 14:30:01 +0000

    [hv222, tvt22] Feat: created firebase text service with addText and query by category functions

--- web-app/src/app/adminDashboard/page.tsx
+++ web-app/src/app/adminDashboard/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { Category, Difficulty, TextService } from "../../services/textservice";
+import { text } from "stream/consumers";
 
 export default function Home() {
+  const textservice = new TextService();
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
---
+++ web-app/src/services/textservice.ts
@@ -0,0 +1,74 @@
+import { getFirestore, collection, getDocs, query, where, Timestamp, addDoc} from 'firebase/firestore/lite';
+import { MY_APP } from '../firebaseconfig.js';
+
+export class TextService {
+    private db;
+
+    constructor() {
+        this.db = getFirestore(MY_APP);
+    }
+
+    async getTexts() {
+        const textsCol = collection(this.db, 'Texts');
+        const textsSnapshot = await getDocs(textsCol);
+        const textsList = textsSnapshot.docs.map(doc => doc.data());
+        console.log(textsList);
+        return textsList;
+    }
+
+    async getTextsByCategory(category: Category) {
+        const q = query(
+            collection(this.db, 'Texts'),
+            where('category', '==', category)
+        )
+        const querySnapshot = await getDocs(q);
+        const textsList = querySnapshot.docs.map(doc => doc.data());
+        console.log(textsList);
+        return textsList;
+    }
+
+    // async getTextsByUserId(userId: string) {}
+
+    async getTextsByDifficulty(difficulty: Difficulty) {}
+
+    async getRandomText(category: Category, difficulty: Difficulty) {}
+
+    /// Add a text to the database 
+    async addText(category_: Category, content_: string, difficulty_: Difficulty, title_: string) {
+        const createdAt_ = Timestamp.now();
+        const wordLength_ = content_.split(' ').length;
+        
+        try {
+            const docRef = await addDoc(collection(this.db, 'Texts'), {
+                title: title_,
+                content: content_,
+                category: category_,
+                createdAt: createdAt_,
+                updatedAt: createdAt_,
+                difficulty: difficulty_,
+                status: 'approved',
+                wordLength: wordLength_
+            });
+            console.log('Document written with ID: ', docRef.id);
+        } catch (error) {
+            console.error('Error adding document: ', error);
+        }
+    }
+
+    async deleteText(textId: string) {}
+
+}
+
+export default new TextService();
+
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = 'medium',
+    HARD = 'hard'
+}
+
+export enum Category {
+    NATURE = 'nature',
+    SCIENCE = 'science',
+    TECHNOLOGY = 'technology',
+}
