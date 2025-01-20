24bdbe92146d540779db6f298d8f2b0bb2916ea4
Hardiv Harshakumar <hardivhk@gmail.com>
2025-01-20 14:05:13 +0000

    [hh1222] Add auth0 config files

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./assets/styles/globals.css";
+import "../../public/assets/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
