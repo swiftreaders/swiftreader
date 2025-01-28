c6522e4aa9d026585432b741bb52af7b9530a80e
Hardiv Harshakumar <hardivhk@gmail.com>
2025-01-28 11:31:19 +0000

    [hh1222] Update userService (re-add functions)

--- web-app/src/services/userService.ts
+++ web-app/src/services/userService.ts
@@ -7,6 +7,10 @@ import {
   updateDoc,
   deleteDoc,
   getDoc,
+  Timestamp,
+  query,
+  where,
+  getDocs,
 } from "firebase/firestore";
 import { app } from "@/../firebase.config";
 import { User } from "@/types/user";
@@ -53,6 +57,69 @@ export const userService = {
     } catch (error) {
       console.error("Error updating user:", error);
       return false;
+    }
+  },
+
+  setReadingGoal: async (goal: number, id: string): Promise<boolean> => {
+    try {
+      await updateDoc(doc(db, "Users", id), { readingGoal: goal });
+      return true;
+    } catch (error) {
+      console.error("Error setting reading goal:", error);
+      return false;
+    }
+  },
+
+  retrieveReadingAmount: async (id: string): Promise<number> => {
+    try {
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+      // Convert the JavaScript Date to a Firestore Timestamp
+      const oneMonthAgoTimestamp = Timestamp.fromDate(oneMonthAgo);
+
+      // Query reading sessions for the user within the last month
+      const sessionsQuery = query(
+        collection(db, "reading_sessions"),
+        where("userId", "==", id),
+        where("startTime", ">=", oneMonthAgoTimestamp)
+      );
+
+      const sessionsSnapshot = await getDocs(sessionsQuery);
+
+      // Calculate the total reading amount
+      let totalReadingAmount = 0;
+      sessionsSnapshot.forEach((doc) => {
+        const sessionData = doc.data();
+        totalReadingAmount += sessionData.endTime - sessionData.startTime || 0;
+      });
+
+      return totalReadingAmount;
+    } catch (error) {
+      console.error("Error retrieving reading amount:", error);
+      throw error;
+    }
+  },
+
+  getReadingGoal: async (id: string): Promise<number> => {
+    try {
+      // Create a reference to the user document
+      const userDocRef = doc(db, "Users", id);
+
+      // Fetch the document data
+      const userDocSnapshot = await getDoc(userDocRef);
+
+      // Check if the document exists and return the reading goal
+      if (userDocSnapshot.exists()) {
+        const userData = userDocSnapshot.data();
+        return userData.readingGoal || 0; // Default to 0 if readingGoal is not set
+      } else {
+        console.error("User document not found");
+        return 0; // Return 0 if the document doesn't exist
+      }
+    } catch (error) {
+      console.error("Error retrieving reading goal:", error);
+      throw error; // Re-throw the error for handling upstream
     }
   },
 };
