575124ff0e09c13795acc84f5e9c506079722484
Hardiv Harshakumar <hardivhk@gmail.com>
2025-02-04 10:53:31 +0000

    [hh1222] update page

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { user, loggedIn } = useAuth();
+  const { user } = useAuth();
 
   if (!user) {
     return (
