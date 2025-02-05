61a13b5899c879254d573260e9e5553d90310a74
Hardiv Harshakumar <hardivhk@gmail.com>
2025-02-05 12:02:29 +0000

    [hh1222] Update next.config.js (Add AUTH0_BASE env variable)

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -8,6 +8,13 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
+  env: {
+    AUTH0_BASE_URL:
+      process.env.AUTH0_BASE_URL ||
+      (process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000"),
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
