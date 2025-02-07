6caf84bae46b52d92cc5beb6073d8faf7192b138
siddhantgadkari <svg21@ic.ac.uk>
2025-02-07 11:26:49 +0000

    [svg21] adminDashboard remove button added

---
+++ web-app/src/app/sandbox/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// type UserInfoSessionWidgetProps = {
+//   sessionNumber: number;
+//   title: string;
+//   genre: string;
+//   difficulty: string;
+//   wpm: number;
+//   comprehensionScore: number;
+//   sessionDate: string; // Example: "01 Feb 2025, 14:00"
+// };
+
+const UserInfoSessionWidget = () => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4 w-full max-w-md border">
+    </div>
+  );
+};
+
+export default UserInfoSessionWidget;
